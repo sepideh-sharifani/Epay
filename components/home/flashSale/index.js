0bcf90bde5a010edc56d8078e158a0e8e29a3ce5
sepideh-sharifani <sepideh.sharifani@gmail.com>
2023-04-26 11:10:21 +0330

    name changed

--- components/home/flashSale/index.js
+++ components/home/flashSale/index.js
@@ -7,7 +7,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper';
 import { flashSaleItems } from '../../../data/data.js';
-import Card from './Cardsss';
+import Card from './Cards';
 
 function FlashSale({ products }) {
 	const discountedProducts = products.filter((product) =>
